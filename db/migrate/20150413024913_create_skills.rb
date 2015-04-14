5c0ffd1d412b58c049b4dc3b6135a7ce4f7af55e
Chris McKnight <cmckni3@gmail.com>
2015-04-14 12:59:38 -0500

    Adds candidate and skill models

---
+++ db/migrate/20150413024913_create_skills.rb
@@ -0,0 +1,11 @@
+class CreateSkills < ActiveRecord::Migration
+  def change
+    create_table :skills do |t|
+      t.string :name
+      t.integer :candidate_id
+
+      t.timestamps null: false
+    end
+    add_index :skills, :candidate_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150413024913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "candidate_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "skills", ["candidate_id"], name: "index_skills_on_candidate_id", using: :btree
 
 end
