a42a11c72c201a1abdf1c05f29d6b9daf7c827cf
Chris McKnight <cmckni3@gmail.com>
2015-11-12 16:39:50 -0600

    Fixes application controller by raising exception on CSRF

--- app/controllers/api/v1/candidates_controller.rb
+++ app/controllers/api/v1/candidates_controller.rb
@@ -1,19 +1,24 @@
-class API::V1::CandidatesController < ApplicationController
-  respond_to :json
+ module API
+   module V1
+     class CandidatesController < ApplicationController
+      protect_from_forgery with: :null_session
+      respond_to :json
 
-  # GET /v1/candidates.json
-  def candidates
-    @candidates = CandidateQuery.all
-    respond_with @candidates
-  end
+      # GET /api/v1/candidates.json
+      def candidates
+        @candidates = CandidateQuery.all
+        respond_with @candidates
+      end
 
-  # GET /v1/candidates_by_skill.json
-  def candidates_by_skill
-    @candidates = CandidateSkillQuery.new(params[:skill_name])
-    if @candidates.valid?
-      respond_with @candidates.search
-    else
-      respond_with @candidates.errors, status: :unprocessable_entity
+      # GET /api/v1/candidates_by_skill.json
+      def candidates_by_skill
+        @candidates = CandidateSkillQuery.new(params[:skill_name])
+        if @candidates.valid?
+          respond_with @candidates.search
+        else
+          respond_with @candidates.errors, status: :unprocessable_entity
+        end
+      end
     end
   end
 end
